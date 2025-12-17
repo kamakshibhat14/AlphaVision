54565adde7033b79dc710164071a9aea3e7bb3e2
kamakshibhat14 <158990592+kamakshibhat14@users.noreply.github.com>
2025-12-17 23:45:16 +0530

    mongodb

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,9 @@
-from pymongo import MongoClient
 import os
+from pymongo import MongoClient
 
 MONGO_URI = os.environ.get("MONGO_URI")
 
-if not MONGO_URI:
-    raise Exception("MONGO_URI not set in environment variables")
-
 client = MongoClient(MONGO_URI)
-db = client["alphabetDB"]
 
+db = client["alphabetDB"]
 users_collection = db["users"]
-scans_collection = db["scans"]
