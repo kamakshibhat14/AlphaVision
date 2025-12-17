87e7138420381a612535dc1bc8421d20369d57e0
kamakshibhat14 <158990592+kamakshibhat14@users.noreply.github.com>
2025-12-18 01:56:39 +0530

    auto update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:5000",
-  withCredentials: true   // ✅ THIS IS THE FIX
+  withCredentials: true,
 });
 
 export default api;
